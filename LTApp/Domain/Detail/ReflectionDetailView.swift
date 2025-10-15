ef65c7c65cd14cf5080631c76599da5c1b2c9b22
lieon <lieoncx@gmail.com>
2025-10-15 11:49:34 +0800

    Update

--- LTApp/Domain/Detail/ReflectionDetailView.swift
+++ LTApp/Domain/Detail/ReflectionDetailView.swift
@@ -8,47 +8,72 @@ struct ReflectionDetailView: View {
     @StateObject var viewModel: ReflectionDetailViewModel
     @EnvironmentObject var homeCoordinator: HomeCoordinator
     @State var showSummary: Bool = false
+    @State var navibarOpacity: CGFloat = 0
+    
+    enum Constants {
+        static let navibarH: CGFloat = 85
+    }
     
     init(viewModel: ReflectionDetailViewModel) {
         self._viewModel = .init(wrappedValue: viewModel)
     }
     
     var body: some View {
-        ZStack {
-            ScrollView {
+        GeometryReader { proxy in
+            ZStack(alignment: .top) {
+                topbar(proxy)
+                contentView(proxy)
+                summaryView
+            }
+            .ignoresSafeArea(edges: .top)
+            .toolbarVisibility(.hidden, for: .navigationBar)
+            .animation(.easeInOut, value: showSummary)
+            
+        }
+    }
+    
+   @ViewBuilder var summaryView: some View {
+        
+        if showSummary, let sumary = viewModel.sumary {
+            SummaryView(summary: sumary, isPresented: $showSummary)
+                .transition(.opacity)
+        }
+    }
+    
+    func contentView(_ proxy: GeometryProxy) -> some View {
+        ScrollView {
+            LazyVStack(spacing: .zero) {
+                titleView
+                totalView
                 LazyVStack(spacing: .zero) {
-                    titleView
-                    totalView
-                    LazyVStack(spacing: .zero) {
-                        ForEach(viewModel.answers, id: \.id) { answer in
-                            DetailAnswerRow(answer: answer)
-                        }
+                    ForEach(viewModel.answers, id: \.id) { answer in
+                        DetailAnswerRow(answer: answer)
                     }
-                    .padding(.horizontal, 32)
                 }
+                .padding(.horizontal, 32)
             }
-            .defaultBackground()
-            .defaultNavigationBar("") {
-                homeCoordinator.pop()
-            }
-            .task {
-                do {
-                    try await viewModel.fetchData()
-                } catch {
-                    
+        }
+        .contentMargins(.top, .init(top: proxy.safeAreaInsets.top + Constants.navibarH, leading: 0, bottom: 0, trailing: 0), for: .scrollContent)
+        .onScrollGeometryChange(for: CGFloat.self, of: { $0.contentOffset.y }, action: { oldValue, newValue in
+            if newValue < 0 {
+                navibarOpacity = 1 - abs(newValue) / (proxy.safeAreaInsets.top + Constants.navibarH)
+            } else {
+                withAnimation(.easeInOut) {
+                    navibarOpacity = 1
                 }
             }
             
-            if showSummary, let sumary = viewModel.sumary {
-                SummaryView(summary: sumary, isPresented: $showSummary)
-                    .transition(.opacity)
+        })
+        .defaultBackground()
+        .zIndex(1)
+       
+        .task {
+            do {
+                try await viewModel.fetchData()
+            } catch {
+                
             }
         }
-        .toolbar(content: {
-            Color.red
-        })
-        .animation(.easeInOut, value: showSummary)
-
     }
     
     var titleView: some View {
@@ -56,12 +81,13 @@ struct ReflectionDetailView: View {
             .textStyle(size: 32)
             .padding(.horizontal, 42)
             .padding(.top, 10)
+            .opacity(1 - navibarOpacity)
     }
     
     @ViewBuilder var totalView: some View {
         if let sumary = viewModel.sumary {
             Button {
-            showSummary.toggle()
+                showSummary.toggle()
             } label: {
                 Text("\(sumary.totalAnswers) answers, \(sumary.daysOver) days ")
                     .textStyle(size: 10, color: AppColor.color(hex: 0xffffff), fontFamily: .poppinsRegular)
@@ -74,26 +100,43 @@ struct ReflectionDetailView: View {
             }
             .padding(.top, 27)
             .padding(.bottom, 24)
-           
-        }
-    }
-}
-
-
-
-public struct FullScreenCoverBackgroundRemovalView: UIViewRepresentable {
-    private class BackgroundRemovalView: UIView {
-        override func didMoveToWindow() {
-            super.didMoveToWindow()
-            superview?.superview?.backgroundColor = .clear
+            
         }
     }
     
-    public init() {}
-    
-    public func makeUIView(context: Context) -> UIView {
-        return BackgroundRemovalView()
+    func topbar(_ proxy: GeometryProxy) -> some View {
+        
+         VStack(spacing: .zero) {
+             AppColor.backgroundPage
+                 .frame(height: proxy.safeAreaInsets.top)
+             
+             HStack(spacing: .zero) {
+                 Button {
+                     
+                 } label: {
+                     Image(.back)
+                         .padding(.leading, 25)
+                 }
+                 Text(viewModel.title)
+                     .lineLimit(2)
+                     .textStyle(size: 24, fontFamily: .feltTipSeniorRegular)
+                     .padding(.vertical, 14)
+                     .padding(.leading, 15)
+                     .padding(.trailing, 24)
+                     .opacity(navibarOpacity)
+                 
+                 Spacer()
+             }
+             
+             Rectangle()
+                 .fill(AppColor.color(hex: 0xCDCDCD))
+                 .frame(height: 1)
+                 .opacity(navibarOpacity)
+         }
+         .background(AppColor.backgroundPage.opacity(navibarOpacity))
+         .frame(height: Constants.navibarH + proxy.safeAreaInsets.top)
+         .zIndex(3)
+        
+        
     }
-    
-    public func updateUIView(_ uiView: UIView, context: Context) {}
 }
