ba2daef02be76911d9115dfa8bdf0421d09f49cb
lieon <lieoncx@gmail.com>
2026-01-08 13:19:44 +0800

    Update

--- app/ltapp_flutter/lib/src/features/calendar/calendar_item_view.dart
+++ app/ltapp_flutter/lib/src/features/calendar/calendar_item_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ltapp_flutter/src/core/theme/app_style.dart';
 import 'package:ltapp_flutter/src/core/theme/icon_name.dart';
 import 'package:ltapp_flutter/src/core/ui_component/svg_asset.dart';
@@ -12,7 +13,7 @@ class CalendarItemView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return buildTwoIconView();
+    return buildMoreThanThreeIconView();
   }
 
   Widget buildDateView() {
@@ -40,7 +41,7 @@ class CalendarItemView extends StatelessWidget {
     final icon1 = item?.reflections.first.icon;
     final icon2 = item?.reflections.last.icon;
     const double top = 18;
-    const double bottom = 0;
+    const double bottom = 10;
     return Stack(
       children: [
         Positioned(top: 4, left: 4, child: buildDateView()),
@@ -82,6 +83,175 @@ class CalendarItemView extends StatelessWidget {
                     ),
                   ),
                 ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildThreeIconView() {
+    final icon1 = item?.reflections.first.icon;
+    final icon2 = item?.reflections[1].icon;
+    final icon3 = item?.reflections.last.icon;
+    const double top = 18;
+    const double bottom = 1;
+    return Stack(
+      children: [
+        Positioned(left: 4, top: 4, child: buildDateView()),
+        Padding(
+          padding: EdgeInsets.only(top: top, bottom: bottom),
+          child: Column(
+            children: [
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 1),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return iconView(icon1, size, size);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 1),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return iconView(icon2, size, size);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 1),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return iconView(icon3, size, size);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildMoreThanThreeIconView() {
+    final icon1 = item?.reflections.first.icon;
+    final icon2 = item?.reflections[1].icon;
+    final icon3 = item?.reflections.last.icon;
+    final remaining = (item?.reflections.length ?? 0) - 3;
+    const double top = 20;
+    const double bottom = 0;
+    const double hp = 0;
+
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: [
+        Positioned(left: 4, top: 4, child: buildDateView()),
+        Positioned(
+          top: top,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return Container(
+                          width: size,
+                          height: size,
+                          color: Colors.red,
+                          child: iconView(icon3, size, size),
+                        ); //;
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return Container(
+                          width: size,
+                          height: size,
+                          color: Colors.blue,
+                          child: iconView(icon3, size, size),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return Container(
+                          width: size,
+                          height: size,
+                          color: Colors.red,
+                          child: iconView(icon3, size, size),
+                        ); //;
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final size =
+                            constraints.maxHeight < constraints.maxWidth
+                            ? constraints.maxHeight
+                            : constraints.maxWidth;
+                        return Container(
+                          width: size,
+                          height: size,
+                          color: Colors.blue,
+                          child: iconView(icon3, size, size),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
