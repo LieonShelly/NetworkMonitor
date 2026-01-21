a89eca5de9e8f09af5c48a0439c9198754f4f507
lieon <lieoncx@gmail.com>
2026-01-21 16:01:25 +0800

    Update

--- app/LTApp/LTApp/Source/Domain/SignIn/AppleIDSignInView.swift
+++ app/LTApp/LTApp/Source/Domain/SignIn/AppleIDSignInView.swift
@@ -73,18 +73,18 @@ struct AppleIDSignInView: View {
         .frame(height: 54)
         .padding(.horizontal, 30)
         .padding(.bottom, 168)
-       .onTapGesture {
-            Task.detached {
-                do {
-                    try await viewModel.login(authorizationCode: "c8c8dbfc39494450eaa3d3ecdde6f381f.0.srxxu.mebgNRevX0x8XD9i9JRq2Q", identityToken: "eyJraWQiOiJIdlZJNkVzWlhKIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLmxpdHRsZS50aGluZ3MiLCJleHAiOjE3NjkwNjMwNjksImlhdCI6MTc2ODk3NjY2OSwic3ViIjoiMDAxNzc0LmZiNmI2MWIyOTkyZTQ2ODM4YmVlMzRlNzgxYTZhMTE0LjEwMjEiLCJjX2hhc2giOiJyUGZjRktNTHJrdEpIOENLWGFQdTBBIiwiZW1haWwiOiJieGJiZGR4eW40QHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc19wcml2YXRlX2VtYWlsIjp0cnVlLCJhdXRoX3RpbWUiOjE3Njg5NzY2NjksIm5vbmNlX3N1cHBvcnRlZCI6dHJ1ZX0.hEFn2105mX03fKUq8gKpiSLIHLTwF3Z5EDnroy0ih28WsevP2G5htqZEMS59xjkRMJ2pNAJDntlSXAGDweKplCeDnxv3d3UifNP75_qUjHVq_gch15v-PNagBxBLyG6Gvx06oxsjAFv4RH75SGyEsjcsgFQwbwO4ABD3ZurgspzeowTRRyOiCMrub3TTSYnpIF9RgxWvfo4ohyGDTEB8OmPgPOw0uju8j6EzjXMxqS23VWTCI27jPCzazeSFAAuY_dRGSsJGnUFrhhnLILda034nXz-QM-8WXoJRdsYqnSMbjpZ5Ql8kZmJrQYFkt2FNSUxac6dm-YkUUefpFXIWyg")
-                    await route()
-                } catch {
-                  await MainActor.run {
-                        showError = true
-                    }
-                }
-            }
-        }
+//       .onTapGesture {
+//            Task.detached {
+//                do {
+//                    try await viewModel.login(authorizationCode: "", identityToken: ")
+//                    await route()
+//                } catch {
+//                  await MainActor.run {
+//                        showError = true
+//                    }
+//                }
+//            }
+//        }
     }
     
     @MainActor
