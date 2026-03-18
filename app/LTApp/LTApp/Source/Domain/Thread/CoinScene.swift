fc08bfbf52b591d87f269675b435da77aba6058a
lieon <lieoncx@gmail.com>
2026-03-18 16:40:59 +0800

    Update

--- app/LTApp/LTApp/Source/Domain/Thread/CoinScene.swift
+++ app/LTApp/LTApp/Source/Domain/Thread/CoinScene.swift
@@ -4,12 +4,11 @@
 //
 //  Created by Renjun Li on 2026/3/17.
 //
-
-import SpriteKit
+import UIComponent
+@preconcurrency import SpriteKit
 
 class CoinScene: SKScene {
     override func didMove(to view: SKView) {
-        // 预热渲染引擎
         let warmUpNode = SKCropNode()
         warmUpNode.maskNode = SKShapeNode(circleOfRadius: 1)
         warmUpNode.position = CGPoint(x: -100, y: -100) // 放在屏幕外
@@ -25,15 +24,12 @@ class CoinScene: SKScene {
     }
 
     func dropCoinsBatch(localPaths: [String], count: Int) {
-        // 1. 先把所有路径转换成纹理对象
         let textures = localPaths.compactMap { path -> SKTexture? in
             guard let image = UIImage(contentsOfFile: path) else { return nil }
             return SKTexture(image: image)
         }
         
-        // 2. 核心：预加载纹理到 GPU
         SKTexture.preload(textures) { [weak self] in
-            // 只有当所有纹理都准备好后，才开始掉落逻辑
             DispatchQueue.main.async {
                 for (index, texture) in textures.enumerated() {
                     DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.15) {
@@ -45,9 +41,19 @@ class CoinScene: SKScene {
     }
 
     private func createSingleCoin(texture: SKTexture) {
-        let coinSize = CGSize(width: 64, height: 64)
+        let containerDimension: CGFloat = 51
+        let iconDimension: CGFloat = 30
+
+        let radius = containerDimension * 0.5
         
-        let radius = coinSize.width * 0.5
+        let textureSize = texture.size()
+        let iconWidthRatio = iconDimension / textureSize.width
+        let iconHeightRatio = iconDimension / textureSize.height
+        
+        let iconScale = min(iconWidthRatio, iconHeightRatio)
+        let finalIconSize = CGSize(width: textureSize.width * iconScale, height: textureSize.height * iconScale)
+        
+    
         
         let cropNode = SKCropNode()
         
@@ -56,13 +62,20 @@ class CoinScene: SKScene {
         mask.strokeColor = .clear
         cropNode.maskNode = mask
         
-        let coinImageNode = SKSpriteNode(texture: texture, size: coinSize)
+        let bgNode = SKShapeNode(circleOfRadius: radius)
+        bgNode.fillColor = UIColor(AppColor.backgroundPage)
+        bgNode.strokeColor = .clear
+        bgNode.zPosition = -1
+        
+        cropNode.addChild(bgNode)
+        
+        let coinImageNode = SKSpriteNode(texture: texture, size: finalIconSize)
         coinImageNode.position = .zero
         cropNode.addChild(coinImageNode)
         
         let borderNode = SKShapeNode(circleOfRadius: radius)
         borderNode.strokeColor = .black
-        borderNode.lineWidth = 4
+        borderNode.lineWidth = 2
         borderNode.fillColor = .clear
         borderNode.zPosition = 1
         cropNode.addChild(borderNode)
