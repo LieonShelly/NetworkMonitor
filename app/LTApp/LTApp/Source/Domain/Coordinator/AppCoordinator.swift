2aba34f7a4ec303ecaad94eff8fccb2dc01622be
lieon <lieoncx@gmail.com>
2026-04-21 16:36:53 +0800

    Update

--- app/LTApp/LTApp/Source/Domain/Coordinator/AppCoordinator.swift
+++ app/LTApp/LTApp/Source/Domain/Coordinator/AppCoordinator.swift
@@ -89,7 +89,7 @@ final class AppCoordinator: ObservableObject, @unchecked Sendable {
     func launch() {
         rootViewProvider.root
             .sink { [weak self] root in
-                self?.root = .home(.init())
+                self?.root = root
             }
             .store(in: &cancellables)
     }
